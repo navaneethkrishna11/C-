e38417dbc9e61af7e6f0ab352b043d1371749800
Navaneeth_Krishna <95477780+navaneethkrishna11@users.noreply.github.com>
2024-08-07 00:00:12 +0530

    Create Sum of n even Numbers.cpp

---
+++ Basics/Sum of n even Numbers.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter the number of digits : ";
+    cin>>n;
+    cout<<n*(n+1);
+}
