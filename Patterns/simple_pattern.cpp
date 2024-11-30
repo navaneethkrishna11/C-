8d0ac28a689d7d10d73730057db18fa48209a7b6
Navaneeth_Krishna <95477780+navaneethkrishna11@users.noreply.github.com>
2024-12-01 02:29:03 +0530

    Create simple_pattern.cpp

---
+++ Patterns/simple_pattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int x,i;
+  for(i=0;i<6;i++)
+  {
+    for(x=0;x<6;x++)
+    {
+      cout << "*";
+    }
+   cout << "\n";
+   }
+}
+
+/*
+******
+******
+******
+******
+*/
